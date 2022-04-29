58994c63d18ef5e883582c4f5ae1c080aa674085
chris <christopherheyn@gmx.net>
2022-04-29 11:18:48 +0200

    Start writing Javadocs

--- src/main/java/myproject/Account.java
+++ src/main/java/myproject/Account.java
@@ -3,19 +3,45 @@ package myproject;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Repräsentation eines Bankaccounts
+ * @author Christopher
+ * @version 29.04.2022
+ */
 public class Account {
 
+    /**
+     * Fortlaufende Bankkontonummer
+     */
     private static int next_account_number = 1;
+
+    /***
+     * Liste mit den erstellten Konten
+     */
     private static ArrayList<Account> account_list = new ArrayList<>();
 
+    /**
+     * Getter-Methode für account_list
+     * @return gibt die Liste mit den erstellten Konten zurück
+     */
     public static ArrayList<Account> getAccount_list() {
         return account_list;
     }
 
+    /**
+     * SetterMethode für account_list
+     * @param account_list
+     */
     public static void setAccount_list(ArrayList<Account> account_list) {
         Account.account_list = account_list;
     }
 
+    /**
+     * Durchsucht die Liste account_list nach dem angefragtem Konto
+     * @param search_number
+     * @return Kontoliste
+     */
     public static Account find_account(int search_number) {
         List<Account> account_list = getAccount_list();
 
@@ -27,6 +53,13 @@ public class Account {
         return null;
     }
 
+    /**
+     * Überweist einen Betrag von einem Ausgagngskonto auf ein Zielkonto
+     * @param sourceaccount Ausgagngskonto, von dem der Betrag abgebucht wird
+     * @param targetaccount Eingangskonto, auf dem der Betrag überwiesen wird.
+     * @param amount Betrag, der überwiesen werden soll
+     * @return
+     */
     private static boolean transfer(int sourceaccount, int targetaccount, double amount) {
 
         Account sourceaccount_object = null;
